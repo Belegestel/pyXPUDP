446863f3eb9763c334722572e9301797e8fd8394
Wojciech Dudziński <wojdud.pl@gmail.com>
2026-04-13 16:54:54 +0200

    Fixed some bugs in callbacks

--- src/pyXPUDP/callbacks.py
+++ src/pyXPUDP/callbacks.py
@@ -13,7 +13,7 @@ class _CallbackHandle():
 
     def remove(self):
         if self._is_valid():
-            self._dispatcher._remove_callback(key, id)
+            self._dispatcher._remove_callback(self._key, self._id)
         else:
             raise Exception('Callback has already been removed, you cannot remove it twice.')
 
@@ -21,22 +21,22 @@ class _CallbackHandle():
 class _CallbackDispatcher():
     def __init__(self, max_thread_workers=None):
         self._id_gen = itertools.count()
-        self._callbacks = dict()
+        self._callbacks = {None: dict()}
         self._executor = concurrent.futures.ThreadPoolExecutor(max_workers=max_thread_workers)
 
     def _add_callback(self, callback, key=None):
         if key not in self._callbacks:
             self._callbacks[key] = dict()
         callback_id = next(self._id_gen)
-        self._callbacks[key][id] = callback
+        self._callbacks[key][callback_id] = callback
         return _CallbackHandle(key, callback_id, self)
 
     def _remove_callback(self, key, id):
         del self._callbacks[key][id]
 
     def _run_callbacks(self, key, value):
-        funcs_to_run = list(self._callbacks[None].items())
-        funcs_to_run.extend(self._callbacks[key].items())
+        funcs_to_run = list(self._callbacks[None].values())
+        funcs_to_run.extend(self._callbacks.get(key, {}).items())
         for f in funcs_to_run:
             self._executor.submit(f, key, value)
 
